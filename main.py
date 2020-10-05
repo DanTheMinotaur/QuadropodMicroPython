3a34727b7431bed82292f7ab7648be83f57848f8
daniel <me@danieldevine.dev>
2020-10-06 00:44:05 +0100

    Cleanup

--- main.py
+++ main.py
@@ -3,21 +3,6 @@ from machine import I2C, Pin
 import uasyncio
 import utils
 
-# INTERNAL_LED = Pin(2, Pin.OUT)
-
-"""
-servos = servo.Servos(I2C(scl=Pin(5), sda=Pin(4))
-for i in range(16):
-    servos.position(i, us=1500)
-"""
-
-# servos = pca9685.Servos(I2C(scl=Pin(5), sda=Pin(4)))
-
-
-# async def move(servo_pos, degrees, period_ms):
-#     servos.position(servo_pos, degrees=degrees)
-#     await uasyncio.sleep_ms(period_ms)
-
 
 class Leg(utils.Utils):
     def __init__(self, upper, middle, lower):
